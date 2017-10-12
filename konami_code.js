e7b66470798daa1c0ebebaac1866098895832f41
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-12 01:44:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  
+
 let index = 0;
 document.body.addEventListener('keydown', onKeyDownHandler(e));
 
-  
+
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-   
+
     if (key === alphabet[index]) {
       index++;
-   
+
       if (index === alphabet.length) {
         alert("Hurray!");
-   
+
         index = 0;
       }
     } else {
