893c050fe861d2b7f67e1f7103132fdd0fac0869
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-12 00:44:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 const body = document.getElementsByTagName("body");
-body.addEventListener('keydown', init())
+body.addEventListener('keydown', init);
 
-function init = (e) => {
+function init(e) {
   // Write your JavaScript code inside the init() function
    const key = parseInt(e.detal || e.which);
 
@@ -24,4 +24,4 @@ function init = (e) => {
         }
   }
 }
-};
+
